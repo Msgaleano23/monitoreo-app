d56d71bb7c3a46837691525e4fc4ee3e8a9f6e43
Msgaleano23 <43222466+Msgaleano23@users.noreply.github.com>
2022-08-20 14:42:10 -0500

    Add files via upload

---
+++ js/deportista.js
@@ -0,0 +1,19 @@
+function capturadp(){
+    let dt = document.getElementsByName("dt").value;
+    let edad = document.getElementById("edad").value;
+    let deporte = document.getElementById("deporte").value;
+    let dias = document.getElementById("dias").value;
+    let tiempo = document.getElementById("tiempo").value;
+    let estrato = document.getElementById("estrato").value;
+    let comidas = document.getElementById("comidas").value;
+    let enfermedad = document.getElementsByName("enfermedad").value;
+
+    alert(dt);
+    alert(edad);
+    alert(deporte);
+    alert(dias);
+    alert(tiempo);
+    alert(estrato);
+    alert(comidas);
+    alert(enfermedad)    
+}
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function captura(){
+    let deporte = document.getElementById("deporte").value;
+    let entrenar = document.getElementById("entrena").value;
+    let competicion = document.getElementById("competencia").value;
+    alert(deporte);
+    alert(entrenar);
+    alert(competicion);
+    console.log(entrenar);
+    console.log(competicion);
+}
