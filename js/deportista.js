1584bf8c0ccb248c6762113441e59f2a8cb10272
Miller Galeano <43222466+Msgaleano23@users.noreply.github.com>
2022-09-10 23:03:16 -0500

    Avance 1

--- js/deportista.js
+++ js/deportista.js
@@ -1,19 +1,12 @@
 function capturadp(){
-    let dt = document.getElementsByName("dt").value;
+    let dt = document.querySelector('input[name=dt]:checked').value;
     let edad = document.getElementById("edad").value;
     let deporte = document.getElementById("deporte").value;
     let dias = document.getElementById("dias").value;
     let tiempo = document.getElementById("tiempo").value;
     let estrato = document.getElementById("estrato").value;
     let comidas = document.getElementById("comidas").value;
-    let enfermedad = document.getElementsByName("enfermedad").value;
+    let enfermedad = document.querySelector("input[name=enf]:checked").value;
 
-    alert(dt);
-    alert(edad);
-    alert(deporte);
-    alert(dias);
-    alert(tiempo);
-    alert(estrato);
-    alert(comidas);
-    alert(enfermedad)    
+    
 }
